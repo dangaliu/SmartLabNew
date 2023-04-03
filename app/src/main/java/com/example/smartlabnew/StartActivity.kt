6c30a3e18d62b0b7785fa3df19ffb5e5770a0227
dangaliu <ii4603399@gmail.com>
2023-04-03 20:26:34 +0300

    Initial commit

---
+++ app/src/main/java/com/example/smartlabnew/StartActivity.kt
@@ -0,0 +1,16 @@
+package com.example.smartlabnew
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.smartlabnew.databinding.ActivityStartBinding
+
+class StartActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityStartBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityStartBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
